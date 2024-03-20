ebe7f5929847b94e613b0633ca32583505ca922b
easyoon <easyoon@wemakeprice.com>
2024-03-20 17:34:29 +0900

    Mock Data 및 컴포넌트 Props 세부 사항 추가 등

--- front/components/CoverList.tsx
+++ front/components/CoverList.tsx
@@ -1,79 +1,83 @@
 import React from "react";
 import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
 
-export default function CoverList() {
+export default function CoverList({ itemList }:{
+  id: number;
+  image?: string;
+  name: string;
+}[]) {
   return (
     <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
-        <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
+        <p className="text-tiny text-white/60 uppercase font-bold">{itemList[0].name}</p>
+        <h4 className="text-white font-medium text-large">Make good food for me</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src="/images/card-example-4.jpeg"
+        src={itemList[0].image}
       />
     </Card>
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-        <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
+        <p className="text-tiny text-white/60 uppercase font-bold">{itemList[1].name}</p>
+        <h4 className="text-white font-medium text-large">Contribute to the day</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src="/images/card-example-3.jpeg"
+        src={itemList[1].image}
       />
     </Card>
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-        <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
+        <p className="text-tiny text-white/60 uppercase font-bold">{itemList[2].name}</p>
+        <h4 className="text-white font-medium text-large">Creates food like a chef</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src="/images/card-example-2.jpeg"
+        src={itemList[2].image}
       />
     </Card>
     <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
       <CardHeader className="absolute z-10 top-1 flex-col items-start">
         <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-        <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+        <h4 className="text-black font-medium text-2xl">{itemList[3].name}</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card example background"
         className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-        src="/images/card-example-6.jpeg"
+        src={itemList[3].image}
       />
       <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
         <div>
-          <p className="text-black text-tiny">Available soon.</p>
-          <p className="text-black text-tiny">Get notified.</p>
+          <p className="text-black text-tiny">Make Easy .</p>
+          <p className="text-black text-tiny">{itemList[4].name}</p>
         </div>
       </CardFooter>
     </Card>
     <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
       <CardHeader className="absolute z-10 top-1 flex-col items-start">
         <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-        <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
+        <h4 className="text-white/90 font-medium text-xl">Your checklist for better food</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Relaxing app background"
         className="z-0 w-full h-full object-cover"
-        src="/images/card-example-5.jpeg"
+        src={itemList[4].image}
       />
       <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
         <div className="flex flex-grow gap-2 items-center">
           <div className="flex flex-col">
-            <p className="text-tiny text-white/60">Breathing App</p>
-            <p className="text-tiny text-white/60">Get a good night's sleep.</p>
+            <p className="text-tiny text-white/60">Healthy Recipe </p>
+            <p className="text-tiny text-white/60">Get a good healthy food.</p>
           </div>
         </div>
       </CardFooter>
