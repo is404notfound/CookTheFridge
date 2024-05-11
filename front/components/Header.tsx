458d230e9b6b6c08e0c393ad4f7f5c9d94c429fc
easyoon <easyoon@wemakeprice.com>
2024-05-12 01:10:49 +0900

    모바일웹용 메뉴 추가

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -1,31 +1,52 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
+import Image from "next/image";
+import Logo from "@/assets/icons/meal-icon.svg"
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const BASE_URL = '/CookTheFridge';
+  const menuItems = [
+    'Home'
+    ,'Ingredients'
+    ,'Cuisines'
+  ];
 
   return (
-    <Navbar shouldHideOnScroll >
+    <Navbar className="font-bold" onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll >
       <NavbarBrand>
-        <p className="font-bold text-inherit">Cook The Fridge</p>
+        <Image className="p-2" src={Logo} alt="Logo" width={40} height={40} />
+        <p>Cook The Fridge</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link href={BASE_URL} color="foreground">   
-            Home
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={`${BASE_URL}/Ingredients`} color="foreground">
-            Ingredients
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={`${BASE_URL}/Cuisines`} color="foreground">
-            Cuisines
-          </Link>
-        </NavbarItem>
+      {/* Web */}
+      <NavbarContent className="hidden sm:flex gap-4 font-bold" justify="center">
+        {
+          menuItems.map((item, index) => (
+            <NavbarItem key={index}>
+              <Link className="text-black" href={`${BASE_URL}/${item === 'Home' ? '' : item}`}>
+                {item}
+              </Link>
+            </NavbarItem>
+          ))
+        }
       </NavbarContent>
+      {/* MWeb */}
+      <NavbarMenuToggle
+        className="md:hidden"
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+      />
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="text-black"
+              href={`${BASE_URL}/${item === 'Home' ? '' : item}`}
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
